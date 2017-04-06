2b4f1c653c1619177233a2f518a459ca98fe83c1
z7shan <z7shan@student.cs.uwaterloo.ca>
2017-04-06 13:55:51 -0400

    add comments to HEncode.cc

--- HEncode.cc
+++ HEncode.cc
@@ -28,6 +28,7 @@ void get_bit(int *arr, int &count, const char c){
   if(count==8) count=0;
 }
 
+// using t as Huffman tree to encode input, return 0 & 1 in a string.
 string encode(Tree &t,vector<int> &input){
   string s;
   for(auto num:input){
@@ -36,21 +37,30 @@ string encode(Tree &t,vector<int> &input){
   return s;
 }
 
+// bin_out outputs encoding into binary
+// Challenges are that C++ (most languages) only can output 1 byte (1 char) at a time 
+// Need to concatenate all bits into bytes intuitively
 void bin_out(const string &s){
   istringstream iss(s);
   char c;
-  int count=0;
-  int arr[8];
+  int count=0;	// counting the bits
+  int arr[8];	// use to contain one byte.
+  
   try {
     while(1){
       for(register int i=0;i<8;i++){
-	if(!(iss>>c)) throw 0;
-	//cerr<<c;
-	get_bit(arr, count, c);
+	if(!(iss>>c)) throw 0;	// when input exhausted, need to "round up" a byte
+	get_bit(arr, count, c);	//convert c into int and put in arr, count++
       }
+
+      // if we have a full byte, we output it.
       putchar(arr[0]<<7|arr[1]<<6|arr[2]<<5|arr[3]<<4|arr[4]<<3|arr[5]<<2|arr[6]<<1|arr[7]);
+    
     }
   } catch(...){
+
+    // input exhausted, round up a byte by filling 0 bit(s).
+
     switch(count){
       case 0:
 	break;
@@ -58,10 +68,10 @@ void bin_out(const string &s){
 	putchar(arr[0]<<7);
 	break;
       case 2:
-	putchar(arr[0]<<7 | arr[1]<<6);
+	putchar(arr[0]<<7|arr[1]<<6);
 	break;
       case 3:
-	putchar(arr[0]<<7 | arr[1]<<6 | arr[2]<<5);
+	putchar(arr[0]<<7|arr[1]<<6|arr[2]<<5);
 	break;
       case 4:
 	putchar(arr[0]<<7|arr[1]<<6|arr[2]<<5|arr[3]<<4);
@@ -77,16 +87,17 @@ void bin_out(const string &s){
 	break;
     }
   }
-
 }
 
 int main(int argc, char* argv[]){
   
+  // check if have enough argument
   if(argc<2){
     cerr<<"Usage: ./HDecode Filename";
     return 0;
   }
   
+  // To hide password input from screen
   //http://stackoverflow.com/questions/6899025/hide-user-input-on-password-prompt
   termios oldt;
   tcgetattr(STDIN_FILENO, &oldt);
@@ -105,12 +116,12 @@ int main(int argc, char* argv[]){
       cerr<<"The password you entered does not match the pervious one."<<endl;
       cerr<<"Try again."<<endl;
     } else {
-      setSeed(enter);
+      setSeed(enter);	// tools.h
       break;
     }
   }
 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);	// clean up
 
   ifstream fs(argv[1]);
   
@@ -120,32 +131,37 @@ int main(int argc, char* argv[]){
   Ascii ascii2;
   vector<int> input;
   int acc = 128;
+
   while(1){
     ascii1=ascii2;
     input.clear();
-    if(count==1) acc=128;
+    if(count==1) acc=128;	// need a sequence of 128 128 256 512 1024 ...
     for(register int i = 0; i<acc; i++){
       fs>>noskipws>>inp;
-      if(fs.fail()) {
-	break;
-      }
+      if(fs.fail()) break;	// works on empty input file
       input.emplace_back(inp);
       int num = (unsigned int) inp;
       ascii2.update(num);
     }
     Tree t(ascii1);
     if(count==0){
-      random_shuffle(t.list.begin(),t.list.end());
+
+      // use the password as a seed to shuffle the first 128 ASCII entries.
+
+      random_shuffle(t.list.begin(),t.list.end());	// dll <algorithm>
     }
-    t.merge_least();
+    t.merge_least();	// tools.h
     bin_out(encode(t,input));
     acc=acc*2;
     count++;
     if(fs.fail()) break;
   }
+
 #ifdef DEBUG
-  t.huffcode();
+  // tools.h
+  t.huffcode();	
   ascii2.hufffreq();
   wpl(t,ascii2);
 #endif
+
 }
