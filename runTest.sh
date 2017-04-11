ecef118b4ca3fe97b1b07c10677d448505ffe6f0
z7shan <z7shan@student.cs.uwaterloo.ca>
2017-04-10 20:07:42 -0400

    change runTest to properly break the test

--- runTest.sh
+++ runTest.sh
@@ -6,7 +6,14 @@ file=$(mktemp)
 echo pwd >> ${in1}
 echo pwd >> ${in1}
 echo pwd > ${in2}
-cat ${in1} | ./HEncode $1 > ${out}
-cat ${in2}| ./HDecode ${out} > ${file}
-diff $1 ${file}
-rm ${in1} ${in2} ${out} ${file}
+cat ${in1} | ./HEncode $1 > ${out} 2> /dev/null
+cat ${in2}| ./HDecode ${out} > ${file} 2> /dev/null
+diff $1 ${file} > /dev/null
+if [ $? -eq 1 ]; then 
+  echo "Test fail!" >&2
+  rm ${in1} ${in2} ${out} ${file}
+  exit 1
+else 
+  echo "Test passed!" >&2
+  rm ${in1} ${in2} ${out} ${file}
+fi
